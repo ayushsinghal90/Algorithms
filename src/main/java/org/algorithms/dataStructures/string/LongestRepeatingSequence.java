d01e991f261c456cccfca825019744c56a17c22c
Ayush Singhal <ayushsinghal998@gmail.com>
2024-01-07 11:09:02 +0530

    adding javadoc for Longest Repeating sequence and Work Wrap

--- src/main/java/org/algorithms/dataStructures/string/LongestRepeatingSequence.java
+++ src/main/java/org/algorithms/dataStructures/string/LongestRepeatingSequence.java
@@ -1,35 +1,63 @@
 package org.algorithms.dataStructures.string;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-class LongestRepeatingSeq {
+/**
+ * Given string str, find the length of the longest repeating subsequence such that it can be found twice in the given string.
+ * <p>
+ * The two identified subsequences A and B can use the same ith character from string str if and
+ * only if that ith character has different indices in A and B.
+ * For example, A = "xax" and B = "xax" then the index of first "x" must be different in the original string for A and B.
+ * <p>
+ * See: <a href="https://www.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1">Question</a>
+ * <p>
+ * Time Complexity: O(N^2).
+ * <p>
+ * Auxiliary Space: O(N^2).
+ *
+ * @author Ayush Singhal
+ */
+class LongestRepeatingSequence {
+    /**
+     * Main function to demonstrate the medianOfArrays method.
+     *
+     * @param args command line arguments (not used in this example)
+     */
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String str = br.readLine().trim();
-            int ans = LongestRepeatingSubsequence(str);
-            System.out.println(ans);
-        }
+        String str = "axxzxy";
+        int ans = LongestRepeatingSubsequence(str);
+        System.out.println(ans);
     }
 
+    /**
+     * Finds the length of the longest repeating subsequence in a given string.
+     *
+     * @param str the input string
+     * @return the length of the longest repeating subsequence
+     */
     public static int LongestRepeatingSubsequence(String str) {
-        // code here
         int n = str.length();
+
+        // Create a 2D matrix to store the lengths of longest repeating subsequences
         int[][] mat = new int[n + 1][n + 1];
+
+        // Fill the matrix using dynamic programming
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= n; j++) {
+                // Base case: If either of the indices is 0, set the value to 0
                 if (i == 0 || j == 0) {
                     mat[i][j] = 0;
                 } else if (i != j && str.charAt(i - 1) == str.charAt(j - 1)) {
+                    // If characters at current indices are equal and indices are different, extend the subsequence
                     mat[i][j] = mat[i - 1][j - 1] + 1;
                 } else {
+                    // If characters are not equal, take the maximum length from the previous cells
                     mat[i][j] = Math.max(mat[i - 1][j - 1], Math.max(mat[i - 1][j], mat[i][j - 1]));
                 }
             }
         }
+
+        // The length of the longest repeating subsequence is stored in the bottom-right cell of the matrix
         return mat[n][n];
     }
 }
--- src/main/java/org/algorithms/dataStructures/string/WordWrap.java
+++ src/main/java/org/algorithms/dataStructures/string/WordWrap.java
@@ -4,7 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Please check the question link for details on the problem.
+ * <p>
+ * See: <a href="https://www.geeksforgeeks.org/problems/word-wrap1646/1">Question</a>
+ * <p>
+ * Time Complexity: O(N^2).
+ * <p>
+ * Auxiliary Space: O(N).
+ *
+ * @author Ayush Singhal
+ */
 class WordWrap {
+    /**
+     * Main function to demonstrate the medianOfArrays method.
+     *
+     * @param args command line arguments (not used in this example)
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine().trim());
@@ -18,31 +34,49 @@ class WordWrap {
         }
     }
 
+    /**
+     * Solves the Word Wrap problem using dynamic programming to minimize the cost
+     * of formatting words in lines with a given maximum line width.
+     *
+     * @param nums: an array representing the lengths of words
+     * @param k:    the maximum line width
+     * @return The minimum cost of formatting words in lines
+     */
     public static int solveWordWrap(int[] nums, int k) {
-        // Code here
-        int n = nums.length, cost;
+        int n = nums.length;
+        int cost;
+
+        // Array to store the minimum cost of formatting words from index i to the end
         int[] dp = new int[n];
         dp[n - 1] = 0;
 
+        // Dynamic programming loop to fill the dp array
         for (int i = n - 2; i >= 0; i--) {
             int currentLen = -1;
             dp[i] = Integer.MAX_VALUE;
+
+            // Iterate through words starting from index i
             for (int j = i; j < n; j++) {
                 currentLen += nums[j] + 1;
 
+                // If current line length exceeds the maximum line width, break the loop
                 if (currentLen > k) break;
 
+                // Calculate the cost of the current line
                 if (j == n - 1) {
-                    cost = 0;
+                    cost = 0; // No cost for the last line
                 } else {
                     cost = ((k - currentLen) * (k - currentLen)) + dp[j + 1];
                 }
 
+                // Update the minimum cost for formatting words from index i
                 if (cost < dp[i]) {
                     dp[i] = cost;
                 }
             }
         }
+
+        // Return the minimum cost for formatting words from the beginning
         return dp[0];
     }
 }
